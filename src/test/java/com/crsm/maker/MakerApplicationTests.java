7fb767726494ea52256f3f487b9d17ef54895685
CcrWrold <1184421183@qq.com>
2019-01-22 04:31:53 +0800

    --使用 pi通道控制树莓派启动关闭拍照

--- src/test/java/com/crsm/maker/MakerApplicationTests.java
+++ src/test/java/com/crsm/maker/MakerApplicationTests.java
@@ -5,27 +5,30 @@ import com.crsm.maker.rabbitmq.HelloSender;
 import com.crsm.maker.user.entity.SysRms;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MakerApplicationTests {
 
     @Autowired
-    private HelloSender helloSender;
+    AmqpTemplate template;
 
     @Test
     public void contextLoads() {
-        SysRms sysRms=new SysRms();
-        sysRms.setId(1);
-        sysRms.setRmsIocn("testIocn");
-        sysRms.setRmsName("testRname");
-        sysRms.setRmsUrl("testUrl");
-        String jsonObject=JSONObject.toJSONString(sysRms);
+        Map map=new HashMap<String,Object>();
+        map.put("order",0); //1：启动 0：关闭
+        map.put("content","TestMessage");
+        map.put("status",200);
+        String jsonObject=JSONObject.toJSONString(map);
         System.out.println(jsonObject);
-        helloSender.send(jsonObject);
+        template.convertAndSend("pi",jsonObject);
     }
 
 }
